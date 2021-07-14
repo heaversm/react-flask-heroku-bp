2cb66bdd03f70db9f17cf07d94ebc61cf4020834
Mike Heavers <mheavers@gmail.com>
2021-07-14 15:02:54 -0700

    add glean with basic metric, and ability to toggle

--- api/api.py
+++ api/api.py
@@ -1,15 +1,39 @@
 import time
 from flask import Flask
+from glean import Glean, load_metrics
+metrics = load_metrics("metrics.yaml")
+
+#allow telemetry to be disabled
+telemetry_enabled = False
+
+Glean.initialize(
+  application_id="react-flask-heroku-bp",
+  application_version="0.1.0",
+  upload_enabled=telemetry_enabled,
+  data_dir=""
+)
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+  metrics.app.loads.add()  # Increment the app loads counter in glean
+  return app.send_static_file('index.html')
 
 @app.route('/api/time')
 def get_current_time():
   return {'time': time.time()}
 
-# if __name__ == '__main__':
-#     flask_app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
+@app.route('/api/toggle_telemetry/<is_enabled>')
+def toggle_telemetry(is_enabled):
+  app.logger.info('telemetry enabled: %s', is_enabled)
+  if is_enabled:
+    telemetry_enabled = is_enabled
+  else:
+    telemetry_enabled = not telemetry_enabled
+
+  if telemetry_enabled:
+    Glean.set_upload_enabled(1)
+  else:
+    Glean.set_upload_enabled(0)
+  return {'telemetryEnabled': telemetry_enabled}
--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
+  const [telemetryEnabled, setTelemetryEnabled] = useState(false);
 
   useEffect(() => {
     fetch("/api/time")
@@ -11,13 +12,27 @@ function App() {
       .then((data) => {
         setCurrentTime(data.time);
       });
+
+    toggleTelemetry();
   }, []);
+
+  const toggleTelemetry = (e, isEnabled = !telemetryEnabled) => {
+    fetch(`/api/toggle_telemetry/${isEnabled}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const isEnabled = data.telemetryEnabled === "true"; //convert to bool
+        setTelemetryEnabled(isEnabled);
+      });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <a onClick={toggleTelemetry}>
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
         <p>The current time is {currentTime}.</p>
+        {telemetryEnabled && <p>Telemetry enabled</p>}
       </header>
     </div>
   );
